296d964df8ecca3c14f48f15988aa8d544e0f785
DavidTorres09 <angeldtm09@gmail.com>
2022-10-26 18:23:49 -0600

    Creacion de archivo

---
+++ src/main/java/com/ANGEL_TORRES_MORA_IPRUEBA_PWYP/AngelTorresMoraIpruebaPwypApplication.java
@@ -0,0 +1,13 @@
+package com.ANGEL_TORRES_MORA_IPRUEBA_PWYP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngelTorresMoraIpruebaPwypApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngelTorresMoraIpruebaPwypApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ANGEL_TORRES_MORA_IPRUEBA_PWYP/AngelTorresMoraIpruebaPwypApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ANGEL_TORRES_MORA_IPRUEBA_PWYP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngelTorresMoraIpruebaPwypApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
